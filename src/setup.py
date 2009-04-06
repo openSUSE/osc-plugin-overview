045bcd0fcf6bf0265708a05a43ffeb14fd68cbe0
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-04-06 16:31:44 +0200

    add file to install module

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='oscpluginoverview',
+      version='0.1',
+      description='osc overview plugin',
+      author='Duncan Mac-Vicar P.',
+      author_email='dmacvicar@suse.de',
+      url='http://en.opensuse.org/Build_Service/osc_plugins/Overview',
+      #packages=['distutils', 'distutils.command'],
+#      package_dir={'': 'src'},
+      packages=['oscpluginoverview'],
+     )
