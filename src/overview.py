081363d832fd04a10c8d17242f586d2316449f64
Duncan Mac-Vicar P <dmacvicar@suse.de>
2017-07-25 09:32:17 +0200

    unused import

--- src/overview.py
+++ src/overview.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import string, sys
+import sys
 from osc import cmdln
 
 #from oscpluginoverview import texttable
